c562b2230c08451cd943a16f70b583bcab8037ff
javier <javier_2790@outlook.com>
2024-09-30 18:48:26 +0200

    feat: send text to sel and execute ok

--- app.py
+++ app.py
@@ -1,13 +1,69 @@
 from flask import Flask
+from flask import jsonify
+from flask import request
 from flask import render_template
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
 
 
 app = Flask(__name__)
 
+app.secret_key = '85c89cace37eb9e4ed12a7848e8c1dff1b938a99cd'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1990'
+app.config['MYSQL_DB'] = 'new_database'
+
+mysql = MySQL(app)
+
+@app.route('/test_db')
+def test_db_connection():
+    try:
+        # Conectar a la base de datos
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM USERS')
+        db_name = cursor.fetchall()
+        
+        # Cerrar el cursor
+        cursor.close()
+
+        # Retornar la base de datos seleccionada como respuesta
+        return jsonify({"status": "Conexión exitosa", "base_de_datos": db_name})
+
+    except MySQLdb.Error as e:
+        return jsonify({"status": "Error", "error": str(e)})
+
+def execute_query(query):
+    if request.method == 'POST':
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        try:
+            print('helllooooo')
+            cursor.execute(query)
+            
+            if query.strip().upper().startswith("SELECT"):
+                results = cursor.fetchall()
+                return results
+            else:
+                
+                cursor.commit()
+                return "Consulta ejecutada correctamente"
+        except mysql.Error as e:
+            return f"Error en la consulta: {e}"
+        finally:
+            cursor.close()
 
 @app.route('/')
 def home():
     return render_template('editor.html')
+
+@app.route('/execute_query', methods=['POST'])
+def execute_query_route():
+    sql_query = request.json.get('query')
+    result = execute_query(sql_query)
+    return jsonify(result)
+
+
 
 
 if __name__ == '__main__':
--- static/js/editor_config.js
+++ static/js/editor_config.js
@@ -1,21 +1,42 @@
 // Inicializar Ace Editor
 var editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
+editor.setTheme("ace/theme/twilight");
 editor.session.setMode("ace/mode/sql");
 document.getElementById('editor').style.fontSize='18px';
 
 function resizeEditor() {
-    var height = window.innerHeight;  // Ocupa toda la altura de la ventana
-    var width = window.innerWidth / 2;  // Ocupa el 50% del ancho de la ventana
+    var height = window.innerHeight;  
+    var width = window.innerWidth / 2;  
 
     document.getElementById('editor').style.height = height + 'px';
     document.getElementById('editor').style.width = width + 'px';
 
-    editor.resize();  // Método de Ace Editor para redimensionar el contenido
+    editor.resize();  
 }
 
-// Llamar a la función cuando se cargue la página
+
+// Función para ejecutar la consulta SQL
+document.getElementById("executeQueryButton").addEventListener("click", function() {
+    
+    var sqlQuery = editor.getValue();
+
+    // Hacer la solicitud POST para ejecutar la consulta
+    fetch('/execute_query', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query: sqlQuery })
+    })
+    .then(response => response.json())
+    .then(data => {
+        
+        document.getElementById("outputText").textContent = JSON.stringify(data, null, 2);
+    })
+    .catch((error) => {
+        document.getElementById("outputText").textContent = "Error: " + error;
+    });
+});
 resizeEditor();
 
-// Redimensionar el editor cuando cambie el tamaño de la ventana
 window.onresize = resizeEditor;
